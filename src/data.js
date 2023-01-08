1080b71841143051bf272100fe9e7ee0a1ff49f4
Amal Medbouhi <92800303+MEDBOUHIAMAL@users.noreply.github.com>
2023-01-08 17:37:44 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,36 +1,14 @@
 export const projects = [
   {
-    title: "portfolio",
-    subtitle: "react",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "food delevery",
+    title: "Android shopping app",
     subtitle: "android ",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
+    image: "./SHOPPING.jpeg",
     link: "https://reactbootcamp.com",
   },
-  {
-    title: "DevChat",
-    subtitle: "android",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
+ 
+  
 ];
 
 export const testimonials = [
